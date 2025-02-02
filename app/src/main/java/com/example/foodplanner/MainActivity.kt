5e47be03f35e139a07cada8c5d27a18bf1f1f53c
hayam-tarek <hayamtareq28@gmail.com>
2025-02-02 02:15:25 +0200

    refactor bottom nav bar

--- app/src/main/java/com/example/foodplanner/MainActivity.kt
+++ app/src/main/java/com/example/foodplanner/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.foodplanner
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -25,33 +25,38 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home_nav_graph -> {
-                    showFragment(R.id.homeNavHost)
-                    true
-                }
-                R.id.favorites_nav_graph -> {
-                    showFragment(R.id.favoritesNavHost)
-                    true
-                }
-                R.id.plan_meals_nav_graph -> {
-                    showFragment(R.id.planMealsNavHost)
-                    true
-                }
-                else -> false
-            }
-        }
+//        bottomNavigationView.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.home_nav_graph -> {
+//                    showFragment(R.id.homeNavHost)
+//                    true
+//                }
+//                R.id.favorites_nav_graph -> {
+//                    showFragment(R.id.favoritesNavHost)
+//                    true
+//                }
+//                R.id.plan_meals_nav_graph -> {
+//                    showFragment(R.id.planMealsNavHost)
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 
-    private fun showFragment(fragmentId: Int) {
-        val homeNavHost = findViewById<FragmentContainerView>(R.id.homeNavHost)
-        val favoritesNavHost = findViewById<FragmentContainerView>(R.id.favoritesNavHost)
-        val profileNavHost = findViewById<FragmentContainerView>(R.id.planMealsNavHost)
-
-        homeNavHost.visibility = if (fragmentId == R.id.homeNavHost) View.VISIBLE else View.GONE
-        favoritesNavHost.visibility = if (fragmentId == R.id.favoritesNavHost) View.VISIBLE else View.GONE
-        profileNavHost.visibility = if (fragmentId == R.id.planMealsNavHost) View.VISIBLE else View.GONE
-    }
+//    private fun showFragment(fragmentId: Int) {
+//        val homeNavHost = findViewById<FragmentContainerView>(R.id.homeNavHost)
+//        val favoritesNavHost = findViewById<FragmentContainerView>(R.id.favoritesNavHost)
+//        val profileNavHost = findViewById<FragmentContainerView>(R.id.planMealsNavHost)
+//
+//        homeNavHost.visibility = if (fragmentId == R.id.homeNavHost) View.VISIBLE else View.GONE
+//        favoritesNavHost.visibility = if (fragmentId == R.id.favoritesNavHost) View.VISIBLE else View.GONE
+//        profileNavHost.visibility = if (fragmentId == R.id.planMealsNavHost) View.VISIBLE else View.GONE
+//    }
 }
