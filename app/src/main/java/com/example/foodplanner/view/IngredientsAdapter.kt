127e0474e141ce14f7289c07db35129335485b58
hayam-tarek <hayamtareq28@gmail.com>
2025-02-10 02:28:30 +0200

    show image in IngredientsAdapter

--- app/src/main/java/com/example/foodplanner/view/IngredientsAdapter.kt
+++ app/src/main/java/com/example/foodplanner/view/IngredientsAdapter.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.foodplanner.R
 
 class IngredientsAdapter(
@@ -18,6 +21,7 @@ Adapter<IngredientsAdapter.ItemsHolder>() {
     class ItemsHolder(val row: View) : RecyclerView.ViewHolder(row) {
         val title: TextView = row.findViewById(R.id.itemTitle)
         val subTitle: TextView = row.findViewById(R.id.itemSubTitle)
+        val ingredientImage: ImageView = row.findViewById(R.id.ingredinentImage)
     }
 
     override fun getItemCount(): Int {
@@ -36,6 +40,11 @@ Adapter<IngredientsAdapter.ItemsHolder>() {
     override fun onBindViewHolder(holder: ItemsHolder, position: Int) {
         holder.title.text = data[position]
         holder.subTitle.text = subData[position]
+        Glide.with(context)
+            .load("https://www.themealdb.com/images/ingredients/${data[position]}-Small.png")
+            .transform(RoundedCorners(25))
+            .placeholder(R.drawable.loading)
+            .into(holder.ingredientImage)
         holder.row.setOnClickListener {
             Toast.makeText(context, data[position], Toast.LENGTH_LONG).show()
         }
